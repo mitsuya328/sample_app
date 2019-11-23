ab60d6419673a10d76bc6cde8c17eb321a7ce506
mitsuya328 <55015500+mitsuya328@users.noreply.github.com>
2019-11-23 18:43:55 +0900

    Add user mailer

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,29 +4,39 @@ FactoryBot.define do
     email { 'michael@example.com' }
     password { 'password' }
     admin { true }
+    activated { true }
+    activated_at { Time.zone.now }
   end
 
   factory :archer, class: User do
     name { 'Sterling Archer' }
     email { 'duchess@example.gov' }
     password { 'password' }
+    activated { true }
+    activated_at { Time.zone.now }
   end
 
   factory :lana, class: User do
     name { 'Lana Kane' }
     email { 'hands@example.gov' }
     password { 'password' }
+    activated { true }
+    activated_at { Time.zone.now }
   end
 
   factory :malory, class: User do
     name { 'Maloy Archer' }
     email { 'boss@example.gov'}
     password { 'password' }
+    activated { true }
+    activated_at { Time.zone.now }
   end
 
   factory :user do
     sequence(:name)  { |n| "User #{n}" }
     sequence(:email) {|n|"user_#{n}@example.com"}
     password { 'password' }
+    activated { true }
+    activated_at { Time.zone.now }
   end
 end
